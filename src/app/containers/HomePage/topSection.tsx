e9c447d09b3d22464baef92c56418e7745fe872b
David Metcalfe <davidmetcal@Davids-MacBook-Pro.local>
2022-02-28 09:58:03 -0600

    added self ping

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -9,6 +9,7 @@ import Blob from '../../../assets/images/blob.svg';
 import Rocket from '../../../assets/images/Rocket-pink.png';
 import { SCREENS } from '../../components/responsive';
 import Button from '../../components/buttons';
+import hsp from 'heroku-self-ping';
 
 const TopSectionContainer = styled.div`
 	min-height: 340px;
@@ -232,6 +233,8 @@ const ButtonsContainer = styled.div`
 `;
 
 export const TopSection = () => {
+	hsp('http://rocketpig.co');
+
 	return (
 		<TopSectionContainer>
 			<LeftContainer>
